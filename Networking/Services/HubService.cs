4a69ddc7cb63b19ff5ee6330dfeb056f4912bb1d
Mateusz Łąpieś <mateusz.lapies@student.pk.edu.pl>
2023-03-20 23:37:02 +0100

    Networking test #6

--- Networking/Services/HubService.cs
+++ Networking/Services/HubService.cs
@@ -1,25 +1,27 @@
 ﻿using Makaretu.Dns;
-using Microsoft.AspNetCore.Hosting.Server;
 using Microsoft.AspNetCore.SignalR.Client;
 using Networking.Data;
 using Networking.Hubs;
 using Serilog;
-using System.Net;
+using System.Linq;
 using System.Net.NetworkInformation;
 
 namespace Networking.Services
 {
     public class HubService
     {
-        public static readonly ILogger logger = Log.ForContext(typeof(HubService));
+        private static readonly string service = "blockchain";
+        private static readonly string instance = Environment.MachineName;
+
+        private static readonly ILogger logger = Log.ForContext(typeof(HubService));
 
         public static HubInstances Instances { get; set; }
 
         private static MulticastService multicastService;
         private static ServiceDiscovery serviceDiscovery;
 
-        private static readonly DomainName instanceName = new (Environment.MachineName);
-        private static readonly DomainName serviceName = new ("_blockchain._tcp");
+        private static readonly DomainName instanceName = new (instance);
+        private static readonly DomainName serviceName = new (string.Format("_{0}._tcp", service));
         private static readonly ServiceProfile serviceProfile = new (instanceName, serviceName, 7281);
 
         public static void Init()
@@ -52,32 +54,32 @@ namespace Networking.Services
 
                 serviceDiscovery.ServiceInstanceDiscovered += (s, e) =>
                 {
-                    if (!e.ServiceInstanceName.Labels.Contains<string>(instanceName.Labels[0]))
+                    string name = e.ServiceInstanceName.ToString();
+                    if (!name.Contains(instance) && name.Contains(service))
                     {
-                        if (e.ServiceInstanceName.Labels.Contains<string>(serviceName.Labels[0]))
-                        {
-                            logger.Information("Service Instance discovered: {name}", e.ServiceInstanceName);
-                            multicastService.SendQuery(e.ServiceInstanceName, type: DnsType.SRV);
-                        }
+                        logger.Information("Service Instance discovered: {name}", e.ServiceInstanceName);
+                        multicastService.SendQuery(e.ServiceInstanceName, type: DnsType.SRV);
                     }
                 };
 
                 multicastService.AnswerReceived += (s, e) =>
                 {
                     IEnumerable<SRVRecord> services = e.Message.Answers.OfType<SRVRecord>();
-                    foreach (SRVRecord service in services)
+                    foreach (SRVRecord srv in services)
                     {
-                        if (!service.Target.ToString().Contains(instanceName.ToString()) && service.Name.ToString().Contains(serviceName.ToString()))
+                        if (!srv.Target.ToString().Contains(instance) && srv.Name.ToString().Contains(service))
                         {
-                            logger.Information("Host {host} for {service} has been discovered", service.Target, service.Name);
-                            multicastService.SendQuery(service.Target, type: DnsType.A);
+                            logger.Information("Host {host} for {service} has been discovered", srv.Target, srv.Name);
+                            multicastService.SendQuery(srv.Target, type: DnsType.A);
                         }
                     }
 
                     IEnumerable<AddressRecord> addresses = e.Message.Answers.OfType<AddressRecord>();
                     foreach (AddressRecord address in addresses)
                     {
-                        if (!address.Name.ToString().Contains(instanceName.ToString()))
+                        string addressName = address.Name.ToString();
+
+                        if (!addressName.Contains(instance) && addressName.Contains(service))
                         {
                             logger.Information("Host {host} at {service} has been discovered", address.Name, address.Address);
                             HubConnection connection = new HubConnectionBuilder()
