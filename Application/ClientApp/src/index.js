42de61e3a93a9c36096ed38ceedb1a50d4cdb802
Mateusz Łąpieś <mateusz.lapies@student.pk.edu.pl>
2023-04-06 11:47:00 +0200

    Clientside server for sync and lock

--- Application/ClientApp/src/index.js
+++ Application/ClientApp/src/index.js
@@ -10,6 +10,8 @@ import reportWebVitals from './reportWebVitals';
 
 import './index.css';
 
+import './proxy';
+
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
